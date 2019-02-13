b9cf2caae6d71966d47c4755584e9364ad83bd59
michaelzhou1987 <39017211+michaelzhou1987@users.noreply.github.com>
2019-02-13 15:09:20 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,56 @@
 </template>
 
 <style lang="scss">
+@charset "utf-8";
+
+/*====================CSS Reset====================*/
+header,footer,canvas,summary,details,nav,section,main,article,aside,figure,figcaption {
+	display: block;
+}
+body,h1,h2,h3,h4,h5,h6,p,a,img,input,ul,li,ol,dl,dt,dd,header,footer,canvas,summary,details,nav,section,main,article,aside,figure,figcaption {
+	padding: 0;
+	margin: 0;
+}
+img {
+	display: block;
+	border: 0;
+}
+ul,ol {
+	list-style: none;
+}
+input {
+	outline: none;
+}
+body {
+	font-size: 12px;
+	color: #3e3e3e;
+	font-family: Arial, Verdana, "微软雅黑", "Heiti SC";
+}
+a {
+	text-decoration: none;
+	color: #909090;
+}
+a:hover {
+	text-decoration: underline;
+	color: #F60;
+}
+/*====================End Reset====================*/
+/*====================Global====================*/
+.g-wrapper {
+	width: 1190px;
+	margin: 0 auto;
+}
+.g-left {
+	float: left;
+}
+.g-right {
+	float: right;
+}
+/*====================End Global====================*/
+</style>
+
+
+<style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
